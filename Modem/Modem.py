469c622605459533958d77a254d912a1745991f2
timmsa5 <timmsa5@uni.coventry.ac.uk>
2024-04-21 03:38:56 +0100

    Add project files.

---
+++ Modem/Modem.py
@@ -0,0 +1,14 @@
+#Please bear with while I figure out how this shit works
+
+import kivy
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+
+    def build(self):
+        return Label(text='Hello world')
+
+if __name__ == '__main__':
+    MyApp().run()
