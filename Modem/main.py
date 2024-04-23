9d1514619778ee4a296974ff15ceb9374f02bd18
timmsa5 <timmsa5@uni.coventry.ac.uk>
2024-04-23 14:57:17 +0100

    Initial buildozer test

--- Modem/main.py
+++ Modem/main.py
@@ -1,5 +1,8 @@
-#Please bear with while I figure out how this shit works
+# Please bear with while I figure out how this shit works
 import kivy
+
+kivy.require('2.3.0')
+
 
 from kivy.app import App
 from kivy.uix.label import Label
@@ -7,7 +10,8 @@ from kivy.uix.label import Label
 class MyApp(App):
 
     def build(self):
-        return Label(text='Hello world')
+        return Label(text='Buildozer Test')
+        # if no mood yet, just open log straight away
 
 if __name__ == '__main__':
     MyApp().run()
