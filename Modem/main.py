da7fa974e7c43843d0f622abb91f836b0762900c
timmsa5 <timmsa5@uni.coventry.ac.uk>
2024-04-28 17:58:10 +0100

    Got main.py running on android device Finished app design Added, renamed, and cropped graphics Got Kivy working and added a few basic functions

--- Modem/main.py
+++ Modem/main.py
@@ -1,17 +1,33 @@
-# Please bear with while I figure out how this shit works
 import kivy
 
 kivy.require('2.3.0')
 
-
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+import sqlite3
 
-class MyApp(App):
-
+class ModemApp(App):
     def build(self):
-        return Label(text='Buildozer Test')
-        # if no mood yet, just open log straight away
+        return ModemScreen()
+
+class ModemScreen(BoxLayout):
+    con = sqlite3.connect('test.db')
+    
+    def readData(self):
+        return 'temp test'
+    
+    def saveData(self, data, type):
+        print(data)
+        # Write the text to a file
+    
+
+        
+#search 
+#log
+#graphs
 
 if __name__ == '__main__':
-    MyApp().run()
+    ModemApp().run()
